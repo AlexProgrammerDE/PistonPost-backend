ed8ef838a7c92e15edc57e98c2ca44c00b23e18b
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-09-30 20:52:08 +0200

    Add cleanup log

--- src/main/java/net/pistonmaster/pistonpost/tasks/CleanTask.java
+++ src/main/java/net/pistonmaster/pistonpost/tasks/CleanTask.java
@@ -2,15 +2,19 @@ package net.pistonmaster.pistonpost.tasks;
 
 import lombok.RequiredArgsConstructor;
 import net.pistonmaster.pistonpost.manager.StaticFileManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.TimerTask;
 
 @RequiredArgsConstructor
 public class CleanTask extends TimerTask {
     private final StaticFileManager manager;
+    private final Logger logger = LoggerFactory.getLogger(CleanTask.class);
 
     @Override
     public void run() {
+        logger.info("Cleaning up files...");
         manager.cleanUnusedFiles();
     }
 }
